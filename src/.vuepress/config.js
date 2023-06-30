f5225a63c8cc5dc9f4ff0c3e1ed8ad6e68243b51
Alicia Garcia <alicia.gr@campeche.tecnm.mx>
2023-06-30 12:37:40 -0600

    definicion dest

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
- 
+  dest: ingenieria-software,
+  base: '/ingenieria-software/',
   title: 'Ingeniería de Software',
   description: 'Contenido digital de la asignatura Ingeniería de Software',
 
